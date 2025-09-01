21f45ccb60bb571266e25c0b14b8d3a814268036
ahmed-592 <ahmmeeddd592@gmail.com>
2025-09-01 20:35:13 +0300

    responsive commit

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -6,8 +6,8 @@ import { Outlet } from 'react-router-dom';
 export default function MainLayout() {
   return <>
   <Navbar/>
-  <div className=' bg-gray-200 py-4'>
-    <div className="container bg-gray-200 mx-auto max-w-4/6 ">
+  <div className='bg-gray-200 py-4'>
+    <div className="bg-gray-200 mx-auto max-w-full sm:max-w-5/6 md:max-w-4/6 ">
     <Outlet></Outlet>
   </div>
   </div>
